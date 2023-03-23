10d7289b327becc6c0e3bfb7e3eb7742531d77d4
BEOMKING <qjawlsqjacks@naver.com>
2023-03-23 13:17:41 +0900

    ModelMapper 의존성 추가

--- src/main/java/com/study/hateoas/HateoasApplication.java
+++ src/main/java/com/study/hateoas/HateoasApplication.java
@@ -1,7 +1,9 @@
 package com.study.hateoas;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HateoasApplication {
@@ -10,4 +12,8 @@ public class HateoasApplication {
         SpringApplication.run(HateoasApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/com/study/hateoas/events/EventController.java
+++ src/main/java/com/study/hateoas/events/EventController.java
@@ -3,6 +3,7 @@ package com.study.hateoas.events;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 import java.net.URI;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,14 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
     private final EventRepository eventRepository;
-
-    public EventController(final EventRepository eventRepository) {
+    private final ModelMapper modelMapper;
+    public EventController(final EventRepository eventRepository, final ModelMapper modelMapper) {
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody final Event event) {
-        final Event savedEvent = eventRepository.save(event);
+    public ResponseEntity createEvent(@RequestBody final EventDto eventDto) {
+        final Event savedEvent = eventRepository.save(modelMapper.map(eventDto, Event.class));
         final URI createdUri = linkTo(EventController.class).slash(savedEvent.getId()).toUri();
         return ResponseEntity.created(createdUri).body(savedEvent);
     }
